2ff30648cabc84f389db5b2b500c0c551dff16ea
Eng Omar <engomaralryan@gmail.com>
2024-07-05 08:57:36 +0300

    add  libs and the workig on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,47 @@
 import MaxWithWrapper from "@/components/MaxWithWrapper";
+import { Check } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-slate-50">
       <section>
-        <MaxWithWrapper></MaxWithWrapper>
+        <MaxWithWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 lg:pb-52 lg:pt-24 xl:pt-32 lg:gap-x-0 xl:gap-x-8 sm:pb-32">
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28" />
+                <Image src="/snake-1.png" className="w-full" alt="snake-1" />
+              </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your Image on a
+                <span className="bg-green-600 px-2 text-white">Custom</span>
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Capture your favorite memories with your own,
+                <span className="font-semibold">one-of-one</span> phone case.
+                CaseCobra allows you to protect your memories, not just your
+                phone case.
+              </p>
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    High-quality, durable material
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />5 year
+                    print guarantee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    Modern iPhone models supported
+                  </li>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </MaxWithWrapper>
       </section>
     </div>
   );
--- src/components/MaxWithWrapper.tsx
+++ src/components/MaxWithWrapper.tsx
@@ -1,5 +1,20 @@
-const MaxWithWrapper = () => {
-  return <div>MaxWithWrapper</div>;
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+const MaxWithWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div
+      className={cn("h-full w-full max-w-screen-xl px-2.5 md:px-20", className)}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default MaxWithWrapper;
