e29c7b2389df46d2a70eddd4db6223ba9ec63a31
Eng Omar <engomaralryan@gmail.com>
2024-07-06 13:01:17 +0300

    add the  reviews sec , auth and some comp

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -16,21 +16,29 @@ const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
       )}
       {...props}
     >
-      <Image
-        src={
-          dark
-            ? "/phone-template-dark-edges.png"
-            : "/phone-template-white-edges.png"
-        }
-        className="pointer-events-none z-50 select-none"
-        alt="phone image"
-      />
+      <div className="imageContainer">
+        <Image
+          src={
+            dark
+              ? "/phone-template-dark-edges.png"
+              : "/phone-template-white-edges.png"
+          }
+          className="pointer-events-none z-50 select-none"
+          alt="phone image"
+          layout="responsive"
+          width={100}
+          height={100}
+        />
+      </div>
 
-      <div className="absolute -z-10 inset-0">
+      <div className="absolute -z-10 inset-0 imageContainer">
         <Image
           src={imgSrc}
           className="object-cover min-w-full min-h-full"
           alt="overlaying phone image"
+          layout="responsive"
+          width={100}
+          height={100}
         />
       </div>
     </div>
